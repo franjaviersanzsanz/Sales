2f535830ba091ca81ede9de4038c90f2a52ccffa
franjaviersanzsanz <40033181+franjaviersanzsanz@users.noreply.github.com>
2018-11-02 00:32:35 +0100

    actualizaicón de archivos

--- Sales.BackEnd/Models/LocalDataContext.cs
+++ Sales.BackEnd/Models/LocalDataContext.cs
@@ -8,5 +8,6 @@ namespace Sales.BackEnd.Models
 {
     public class LocalDataContext:DataContext
     {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
     }
 }
